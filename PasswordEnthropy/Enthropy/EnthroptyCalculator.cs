abf2610ea5052c64235d1e22dbbcb5e03ae2e02c
Crosby-Titan <mazafaka2018huy@gmail.com>
2023-11-06 16:33:32 +0300

    NORM

---
+++ PasswordEnthropy/Enthropy/EnthroptyCalculator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordEnthropy.Enthropy
+{
+    public class EnthropyCalculator
+    {
+        private int _L;
+        private int _R;
+        private double _E;
+        public double GetEnthropyValue { get { return _E; } }
+
+        public double Calculate(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return 0d;
+            if (!ValidatePassword(password))
+                return 0d;
+
+            InitializeValues(password);
+
+            _E = CalculateEnthropy();
+
+            return _E;
+        }
+
+        private double CalculateEnthropy()
+        {
+            return (_L * Math.Log2(_R));
+        }
+
+        private void InitializeValues(string password)
+        {
+            _R = ContainsDigit(password) ? 10 : 0;
+
+            _R += password.Any((ch) =>
+            {
+                if (char.IsBetween(ch, 'a', 'z'))
+                    return true;
+                else
+                    return false;
+            }) ? 26 : 0;
+
+            _R += password.Any((ch) =>
+            {
+                if (char.IsBetween(ch, 'A', 'Z'))
+                    return true;
+                else
+                    return false;
+            }) ? 26 : 0;
+
+            _L = password.Length;
+        }
+
+        private bool ValidatePassword(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsDigit(password[i]))
+                    continue;
+
+                if ((char.IsLetter(password[i])))
+                {
+
+                    if (!(char.IsBetween(password[i], 'a', 'z') || char.IsBetween(password[i], 'A', 'Z')))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private bool ContainsDigit(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsDigit(password[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- PasswordEnthropy/MainWindow.xaml.cs
+++ PasswordEnthropy/MainWindow.xaml.cs
@@ -20,9 +20,82 @@ namespace PasswordEnthropy
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Enthropy.EnthropyCalculator calculator = new();
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTable();
         }
+
+        private void InitializeTable()
+        {
+            EnthropyTable.Columns.Add(new DataGridTextColumn
+            {
+                Header = "Значение",
+                Binding = new Binding("Value")
+            });
+            EnthropyTable.Columns.Add(new DataGridTextColumn
+            {
+                Header = "Степень",
+                Binding = new Binding("Cathegory")
+            });
+
+            var values = new object[]
+            {
+                new EnthropyTable{Value="0-24",Cathegory="Очень слабый пароль"},
+                new EnthropyTable{Value="25-49",Cathegory="Слабый пароль"},
+                new EnthropyTable{Value="50-74",Cathegory="Хороший пароль"},
+                new EnthropyTable {Value = "75-100", Cathegory = "Отличный пароль" },
+            };
+
+            foreach (var value in values)
+            {
+                EnthropyTable.Items.Add(value);
+            }
+
+            EnthropyTable.IsReadOnly = true;
+            EnthropyTable.SelectionMode = DataGridSelectionMode.Single;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(InputPassword.Text))
+            {
+                MessageBox.Show("Не введён пароль!");
+            }
+
+            double enthropyValue = calculator.Calculate(InputPassword.Text);
+
+            Output.Content = $"Результат: {GetRank(enthropyValue)} ({Math.Round(enthropyValue,1)})";
+        }
+
+        private string? GetRank(double enthropyValue)
+        {
+            if (enthropyValue >= 0 && enthropyValue <= 24)
+            {
+                return (EnthropyTable.Items[0] as EnthropyTable)?.Cathegory;
+            }
+            else if (enthropyValue >= 25 && enthropyValue <= 49)
+            {
+                return (EnthropyTable.Items[1] as EnthropyTable)?.Cathegory;
+            }
+            else if (enthropyValue >= 50 && enthropyValue <= 74)
+            {
+                return (EnthropyTable.Items[2] as EnthropyTable)?.Cathegory;
+            }
+            else if (enthropyValue >= 75 && enthropyValue <= 100)
+            {
+                return (EnthropyTable.Items[3] as EnthropyTable)?.Cathegory;
+            }
+            else
+                return string.Empty;
+        }
+
+    }
+
+    class EnthropyTable
+    {
+        public string Value { get; set; }
+        public string Cathegory { get; set; }
     }
 }
